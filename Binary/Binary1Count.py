ac0f9c54020f17b73b511cd6ff0f1798c875fb12
Kdoor <BrianKdoor@gmail.com>
2022-01-27 21:20:54 -0600

    Create Binary1Count.py
    Added in the Binary Count problem

---
+++ Binary/Binary1Count.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        num_of_1s = 0
+        
+        for i in range(32):
+            num_of_1s += (n & 1)
+            n = n >> 1
+            
+        return num_of_1s
